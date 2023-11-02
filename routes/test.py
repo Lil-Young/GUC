d39d8a0667aed3b0d90d35992d29a584a2d0531b
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2023-11-02 16:35:58 +0900

    update

---
+++ routes/test.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from db.connection import get_db
+from crud import crud_test
+from apis import test # main logic
+
+router = APIRouter(
+    prefix="/items" # url 앞에 고정적으로 붙는 경로추가
+)
+
+@router.get("/test_route")
+def test_index(db: Session = Depends(get_db)):
+    res = test.test_index(db=db) # apis 호출
+    return {"res":res} # 결과
